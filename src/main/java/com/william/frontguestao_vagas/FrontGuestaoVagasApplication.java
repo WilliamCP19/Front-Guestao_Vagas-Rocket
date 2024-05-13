30c240dc91dc825e6f156c413a19cdfce1f1ad02
WilliamCP <williamcruzpires@alunos.utfpr.edu.br>
2024-05-13 15:44:44 -0300

    Primeiro Commit - Criando o projeto

---
+++ src/main/java/com/william/frontguestao_vagas/FrontGuestaoVagasApplication.java
@@ -0,0 +1,13 @@
+package com.william.frontguestao_vagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontGuestaoVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontGuestaoVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/frontguestao_vagas/FrontGuestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.frontguestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontGuestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
