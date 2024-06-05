23a9975a9bac833e34ff4269e9a351f1b15280f4
WilliamCP <williamcruzpires@alunos.utfpr.edu.br>
2024-06-05 17:58:55 -0300

    Enviando Dados  de um Form para Controller

--- src/main/java/com/william/frontguestao_vagas/PrimeiraPaginaController.java
+++ src/main/java/com/william/frontguestao_vagas/PrimeiraPaginaController.java
@@ -3,6 +3,7 @@ package com.william.frontguestao_vagas;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,12 @@ public class PrimeiraPaginaController {
     @GetMapping("/login")
     public String logarCandidato () {
         return "candidato/login";
+    }
+
+    @PostMapping("/create")
+    public String cadastrarUsuario (String email, Model model) {
+        System.out.println("Email: "+email);
+        model.addAttribute("mensagemController", "Candidato cadastrado");
+        return "PrimeiraPagina";
     }
 }
